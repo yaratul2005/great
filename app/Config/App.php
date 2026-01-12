54234f23087b837cbac1844f8fa8c140656a0b65
yaratul2005 <ratul41g@gmail.com>
2026-01-12 22:18:10 +0600

    proxima

--- app/Config/App.php
+++ app/Config/App.php
@@ -5,33 +5,17 @@ namespace App\Config;
 class App {
     // Detect base URL automatically
     public static function getBaseUrl() {
-        // Simple detection for protocol
+        // HARDCODED for PRODUCTION stability
+        // Change this if you move domains
+        return "https://great10.xyz";
+        
+        /* Auto-detection (Backup)
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
         $host = $_SERVER['HTTP_HOST'];
-        
-        // Detect script path (subdirectory)
-        // If script is at /great/public/index.php, we want /great
-        // If script is at /index.php, we want / (empty)
-        
         $scriptDir = dirname($_SERVER['SCRIPT_NAME']);
-        // Normalize
         $scriptDir = str_replace('\\', '/', $scriptDir);
-        
-        // If public is in the path, remove it to get app root URL for user facing links IF we are rewriting to public
-        // BUT, our Current structure is:
-        // /great/ -> redirects to /great/public/ -> index.php routes relative to internal
-        
-        // Actually, easiest way for XAMPP + Rewrite is:
-        // We want the URL that points to the folder containing .htaccess
-        // which defaults to just "/" or "/great/"
-        
-        // Let's hardcode for now to ensure stability if auto-detection is flaky, 
-        // OR better: use relative base.
-        
-        // Let's try to detect the "public" part and strip it if we are hiding it via htaccess
-        // But since we are using /great/ in browser, that's our base.
-        
         $path = str_replace('/public', '', $scriptDir);
         return $protocol . $host . rtrim($path, '/');
+        */
     }
 }
